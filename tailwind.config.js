38eb9baeab4669f2cdf9c1ff2a195d4a17697451
Raidah Fairooz <rfair008@ucr.edu>
2023-10-09 14:51:29 -0700

    added winc colors and font

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,21 @@ module.exports = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        urbanist: ["var(--font-urbanist)"],
+      },
+      colors: {
+        winc: {
+          black: "#203E4E",
+          pink: "#FA6C5E",
+          orange: "#FFB388",
+          yellow: "#FCDCA7",
+          beige: "#F7EEE3",
+          white: "#FFFFFF",
+        },
+      },
+    },
   },
   plugins: [],
 };
