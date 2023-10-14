78ca637fb57ef35e9b23cfebaabbb1cbbfb1663f
kelly <kelly@Kellys-MacBook-Air-2.local>
2023-10-14 13:16:17 -0700

    create a frame component

--- src/components/about/Frame.js
+++ src/components/about/Frame.js
@@ -1,7 +1,15 @@
 import React from "react";
+import Image from "next/image";
 
-const Frame = () => {
-  return <div>Frame</div>;
+const Frame = ({ picture }) => {
+  return (
+    <div>
+      <Image
+        className="shadow-[-50px_-50px_0px_0px] shadow-winc-pink"
+        src={picture}
+      />
+    </div>
+  );
 };
 
 export default Frame;
