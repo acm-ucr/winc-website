b67eb7f13088e66af44ff428a4e1a204abff9f79
Ramya Madugula <ramyamadugula@Roopas-MacBook-Pro-2.local>
2023-10-16 19:33:56 -0700

    Event Card Issue Ramya

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,19 @@
-import Title from "@/components/Title";
-const Home = () => {
+"use client";
+import React from "react";
+
+import Event from "@/components/events/Event";
+const Events = () => {
   return (
-    <>
-      <Title text="Home" />
-    </>
+    <div className={"rounded"}>
+      <Event
+        title="Life in the Industry: PM"
+        location="zoom: link"
+        start="7:00PM"
+        color="bg-winc-pink"
+        description="For our second installment of the Life in the Industry series: Learn all about the role as a Product Manager, from the details of the day-to-day work schedule to the ranking status, salaries, and the people you work with in this position."
+      />
+    </div>
   );
 };
 
-export default Home;
+export default Events;
--- src/components/events/Event.js
+++ src/components/events/Event.js
@@ -1,7 +1,39 @@
+"use client";
 import React from "react";
-
-const Event = () => {
-  return <div>Event</div>;
+// style the component here
+const Event = ({ title, location, start, color, description }) => {
+  return (
+    <div className={"flex rounded-lg flex-row font-urbanist p-4"}>
+      <div className={color}>
+        <div className={"flex text-center text-winc-white flex-col p-5"}>
+          <div className={"text-5xl font-semibold"}>FEB 29</div>
+          <div className={"font-extralight"}>{start}</div>
+        </div>
+      </div>
+      <div className="flex flex-col bg-winc-beige p-4 text-winc-black ">
+        <div className="text-3xl font-semibold">{title}</div>
+        <div className="text ">{location}</div>
+        <div className="text">{description}</div>
+      </div>
+    </div>
+  );
 };
 
 export default Event;
+
+// // style the component here
+// const Stat = ({ icon, stat, type }) => {
+//   return (
+//     <div
+//       className={`flex flex-row min-w-fit w-full justify-center items-center rounded-full border-8 border-winc-yellow ring-8 ring-winc-orange m-3 text-winc-pink bg-winc-white`}
+//     >
+//       {icon}
+//       <div className="flex flex-col items-center w-11/12">
+//         <div className="text-3xl font-bold">{stat}</div>
+//         <div className="text-winc-black text-lg font-thin">{type}</div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Stat;
