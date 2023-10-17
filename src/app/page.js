398a1e6c3176947b73449ffb509ee92ae651da2d
kelly <kelly@Kellys-MacBook-Air-2.local>
2023-10-17 09:58:53 -0700

    passed in x and y into frame component and checked for boxshadow repositioning

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,8 @@
 import Title from "@/components/Title";
-import Frame from "@/components/about/Frame";
-import about1 from "@/public/assets/about1.webp";
-import about2 from "@/public/assets/about2.webp";
-
 const About = () => {
   return (
     <>
       <Title text="About" />
-      <Frame x={20} y={-20} picture={about1} />
-      <Frame x={-20} y={20} picture={about2} />
     </>
   );
 };
