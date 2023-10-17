94e2a3ec553a02efc2f6fdf92b623ac9c5d1cacd
kelly <kelly@Kellys-MacBook-Air-2.local>
2023-10-17 09:52:51 -0700

    passed in x and y into frame component and checked for boxshadow repositioning

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,16 @@
 import Title from "@/components/Title";
-const Home = () => {
+import Frame from "@/components/about/Frame";
+import about1 from "@/public/assets/about1.webp";
+import about2 from "@/public/assets/about2.webp";
+
+const About = () => {
   return (
     <>
-      <Title text="Home" />
+      <Title text="About" />
+      <Frame x={20} y={-20} picture={about1} />
+      <Frame x={-20} y={20} picture={about2} />
     </>
   );
 };
 
-export default Home;
+export default About;
--- src/components/about/Frame.js
+++ src/components/about/Frame.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Image from "next/image";
 
-const Frame = ({ picture }) => {
+const Frame = ({ x, y, picture }) => {
   return (
     <div className="w-fit h-fit">
       <Image
-        className="shadow-[-50px_-50px_0px_0px] shadow-winc-pink"
+        style={{ boxShadow: `${x}px ${y}px 0px 0px #FA6C5E` }}
         src={picture}
+        width={375}
+        height={375}
       />
     </div>
   );
