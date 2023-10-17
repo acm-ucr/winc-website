d10cb7fd608be57daf66c9a6bcde0687c214d317
Jayanth Samala <jayanthsamala@MacBook-Air-466.local>
2023-10-16 23:54:57 -0700

    jayanth-samala/allPages/Button

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,8 @@
 import Title from "@/components/Title";
-import Button from "@/components/Button";
 const Home = () => {
   return (
     <>
       <Title text="Home" />
-      <Button backgroundColor="#3498db" textColor="white" text="Click me" />
     </>
   );
 };
