b945045332b9d79f7dec7849c563f646abe50c93
kelly <kelly@Kellys-MacBook-Air-2.local>
2023-10-21 17:43:00 -0700

    updated we component

--- src/components/about/We.js
+++ src/components/about/We.js
@@ -7,26 +7,20 @@ import Title from "@/components/Title";
 
 const We = () => {
   return (
-    <Row className="text-winc-black flex justify-between items-center pt-28">
-      <Col sm={6} className="flex justify-center items-center">
+    <Row className="text-winc-black flex items-center font-urbanist py-28 px-20">
+      <Col className="flex items-center justify-center">
         <Frame x={-40} y={-40} picture={about1} />
       </Col>
-      <Col sm={6} className="text-left flex justify-center items-center">
-        <div>
-          <div className="pb-4">
-            <Title text="What do we do?" />
-          </div>
-          <div className="pl-5">
-            <div className="bg-winc-pink w-14 h-1"></div>
-            <p className="flex font-urbanist text-winc-black font-normal text-2xl pt-14 pr-24">
-              We host a plethora of events. Whether you want to ace that coding
-              interview with our workshops, or want to attend speaker panels
-              with some of the most intelligent, down-to-earth people in the
-              industry, or maybe you just want to a hang out with a community,
-              WINC is the place for you.
-            </p>
-          </div>
-        </div>
+      <Col className="text-left flex flex-col">
+        <Title text="What do we do?" />
+        <div className="bg-winc-pink w-14 h-1 mt-6 "></div>
+        <p className="flex text-winc-black font-normal text-2xl w-11/12 pt-20">
+          We host a plethora of events. Whether you want to ace that coding
+          interview with our workshops, or want to attend speaker panels with
+          some of the most intelligent, down-to-earth people in the industry, or
+          maybe you just want to a hang out with a community, WINC is the place
+          for you.
+        </p>
       </Col>
     </Row>
   );
