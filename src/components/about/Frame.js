5f69ffd92611c82e6299effee497bd711acd7626
kelly <kelly@Kellys-MacBook-Air-2.local>
2023-10-21 18:39:28 -0700

    addressed frame comments

--- src/components/about/Frame.js
+++ src/components/about/Frame.js
@@ -3,12 +3,10 @@ import Image from "next/image";
 
 const Frame = ({ x, y, picture }) => {
   return (
-    <div className="w-fit h-fit pl-24">
+    <div className="w-fit h-fit">
       <Image
         style={{ boxShadow: `${x}px ${y}px 0px 0px #FA6C5E` }}
         src={picture}
-        width={593}
-        height={501}
       />
     </div>
   );
