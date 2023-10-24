73f1fa8329c5fc5e49c98b3ca163b721693df27c
Mailyn Nguyen <ngu.mailyn.yen@gmail.com>
2023-10-23 19:56:30 -0700

    merged dev

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,20 +1,10 @@
-import Title from "@/components/Title";
 import We from "@/components/about/We";
-<<<<<<< HEAD
 import What from "@/components/about/What";
-
-const About = () => {
-  return (
-    <>
-      <Title text="About" />
-      <What />
-=======
 import Services from "@/components/about/Services";
 const About = () => {
   return (
     <>
-      <Title text="About" textcolor="text-winc-black" />
->>>>>>> 5539469efa23b88522499972878cd1d632e9216c
+      <What />
       <We />
       <Services />
     </>
