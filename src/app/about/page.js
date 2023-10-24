53aba9a4c7fb60d92ce133ce20c41d003db86374
Mailyn Nguyen <ngu.mailyn.yen@gmail.com>
2023-10-23 19:41:27 -0700

    added what component

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,10 +1,13 @@
 import Title from "@/components/Title";
 import We from "@/components/about/We";
 import Services from "@/components/about/Services";
+import What from "@/components/about/What";
+
 const About = () => {
   return (
     <>
       <Title text="About" textcolor="text-winc-black" />
+      <What />
       <We />
       <Services />
     </>
