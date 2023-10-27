9aaa2cf976f70084f02239c588473b5b83bdd6fc
Mailyn Nguyen <ngu.mailyn.yen@gmail.com>
2023-10-27 13:04:36 -0700

    changed Empower comp

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,15 +1,14 @@
 import We from "@/components/about/We";
 import What from "@/components/about/What";
 import Services from "@/components/about/Services";
-import What from "@/components/about/What";
+import Title from "@/components/Title";
 
 const About = () => {
   return (
     <>
-<<<<<<< HEAD
+
       <Title text="About" textcolor="text-winc-black" />
-=======
->>>>>>> 2673cc5bab349c7f366334340389c25f3666a3c6
+
       <What />
       <We />
       <Services />
--- src/components/home/Empower.js
+++ src/components/home/Empower.js
@@ -7,8 +7,8 @@ const Empower = () => {
       <Title text="Empowering Women in Tech" textcolor="text-winc-black" />
       <div className="bg-winc-pink w-14 h-1 mt-4"></div>
       <p className="w-2/5 mt-3 text-center text-3xl text-winc-black">
-        We're dedicated to empowering and supporting a network of leading women
-        in tech at the University of California, Riverside. We're UCR Women in
+        We&lsquore dedicated to empowering and supporting a network of leading women
+        in tech at the University of California, Riverside. We&lsquore UCR Women in
         Computing.
       </p>
     </div>
