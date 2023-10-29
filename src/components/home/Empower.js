2edc05195898c4d29e76cadc807d43cc9b0e685f
Mailyn Nguyen <ngu.mailyn.yen@gmail.com>
2023-10-29 12:43:07 -0700

    empower

--- src/components/home/Empower.js
+++ src/components/home/Empower.js
@@ -7,9 +7,9 @@ const Empower = () => {
       <Title text="Empowering Women in Tech" textcolor="text-winc-black" />
       <div className="bg-winc-pink w-14 h-1 mt-4"></div>
       <p className="w-2/5 mt-3 text-center text-3xl text-winc-black">
-        We&lsquore dedicated to empowering and supporting a network of leading women
-        in tech at the University of California, Riverside. We&lsquore UCR Women in
-        Computing.
+        We&#39;re dedicated to empowering and supporting a network of leading
+        women in tech at the University of California, Riverside. We&#39;re UCR
+        Women in Computing.
       </p>
     </div>
   );
