c2d51aa8578d911c8202e1533a9f267d45b9aacd
Mailyn Nguyen <ngu.mailyn.yen@gmail.com>
2023-10-29 12:28:00 -0700

    merge conflict

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,15 +1,10 @@
 import We from "@/components/about/We";
 import What from "@/components/about/What";
 import Services from "@/components/about/Services";
-import What from "@/components/about/What";
 
 const About = () => {
   return (
     <>
-<<<<<<< HEAD
-      <Title text="About" textcolor="text-winc-black" />
-=======
->>>>>>> 2673cc5bab349c7f366334340389c25f3666a3c6
       <What />
       <We />
       <Services />
