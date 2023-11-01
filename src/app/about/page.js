8ec5ffb45ded7d17802bf4294686f780a7aa3a8e
Mailyn Nguyen <ngu.mailyn.yen@gmail.com>
2023-11-01 12:46:47 -0700

    correct about/page.js

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,13 +1,9 @@
 import We from "@/components/about/We";
 import What from "@/components/about/What";
 import Services from "@/components/about/Services";
-import Title from "@/components/Title";
-
 const About = () => {
   return (
     <>
-      <Title text="About" textcolor="text-winc-black" />
-
       <What />
       <We />
       <Services />
