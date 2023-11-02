7b04229dd2bf66f291f14c6d219902d0a2b9c2ef
Raidah Fairooz <rfair008@ucr.edu>
2023-11-01 17:19:02 -0700

    fixed empower bugs

--- src/components/home/Empower.js
+++ src/components/home/Empower.js
@@ -5,11 +5,14 @@ import Col from "react-bootstrap/Col";
 
 const Empower = () => {
   return (
-    <Row>
-      <Col className="flex justify-center items-center flex-col">
+    <Row className="flex items-center justify-center font-urbanist">
+      <Col></Col>
+      <Col className="flex flex-col text-center justify-center">
         <Title text="Empowering Women in Tech" textcolor="text-winc-black" />
-        <div className="bg-winc-pink w-14 h-1 mt-4"></div>
-        <p className="w-2/5 mt-3 text-center text-3xl text-winc-black">
+        <div className="flex justify-center">
+          <div className="bg-winc-pink w-14 h-1 mt-4"></div>
+        </div>
+        <p className="w-11/12 mt-3 font-normal text-center text-3xl text-winc-black">
           We&#39;re dedicated to empowering and supporting a network of leading
           women in tech at the University of California, Riverside. We&#39;re
           UCR Women in Computing.
