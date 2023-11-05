046f07741d2e798be2891ff60e65a0524cce2977
Roshini <roshinila@gmail.com>
2023-11-04 22:58:07 -0700

    "Made props"

--- src/components/home/Service.js
+++ src/components/home/Service.js
@@ -8,25 +8,24 @@ import React from "react";
   socialMedia
 */
 
-const Service = ({icon, name, description, socialMedia}) => {
+const Service = ({ icon, name, description, socialMedia }) => {
   return (
     <div className="bg-winc-yellow w-11/12 h-96 rounded-lg flex items-center justify-center">
       <div className="flex flex-column items-center w-11/12">
-        <div className="text-winc-pink" size="45" /> {icon} </div>
-        <h1 className="text-winc-pink  text-center text-3xl mb-4 justify-center font-bold">
-          {name}
-        </h1>
-        <p className="text-winc-pink text-center  text-xl">
-          {description}
-        </p>
+        <div className="text-winc-pink" size="45" /> {icon}{" "}
+      </div>
+      <h1 className="text-winc-pink  text-center text-3xl mb-4 justify-center font-bold">
+        {name}
+      </h1>
+      <p className="text-winc-pink text-center  text-xl">{description}</p>
 
-        {/* Smaller Rectangle */}
-        <div className="bg-winc-pink w-187 h-57 mt-4 rounded-lg flex items-center justify-center">
-          <div className="text-winc-yellow text-3xl justify-center font-light">
-            {socialMedia}
-          </div>
+      {/* Smaller Rectangle */}
+      <div className="bg-winc-pink w-187 h-57 mt-4 rounded-lg flex items-center justify-center">
+        <div className="text-winc-yellow text-3xl justify-center font-light">
+          {socialMedia}
         </div>
       </div>
+    </div>
   );
 };
 
