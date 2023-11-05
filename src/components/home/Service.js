f14246b197e9ce6df994ef855182190b99d79a82
Roshini <roshinila@gmail.com>
2023-11-04 22:57:31 -0700

    Fixed into props and made changes

--- src/components/home/Service.js
+++ src/components/home/Service.js
@@ -1,27 +1,32 @@
 "use client";
 import React from "react";
-import { HiCodeBracket } from "react-icons/hi2";
-const Service = () => {
+/*
+  Componenets in Service Card:
+  icon
+  name
+  description
+  socialMedia
+*/
+
+const Service = ({icon, name, description, socialMedia}) => {
   return (
-    <div className="bg-winc-yellow w-96 h-96 rounded-lg  flex items-center justify-center">
+    <div className="bg-winc-yellow w-11/12 h-96 rounded-lg flex items-center justify-center">
       <div className="flex flex-column items-center w-11/12">
-        <HiCodeBracket className="text-winc-pink" size="45" />
-        <div></div>
+        <div className="text-winc-pink" size="45" /> {icon} </div>
         <h1 className="text-winc-pink  text-center text-3xl mb-4 justify-center font-bold">
-          WORKSHOPS
+          {name}
         </h1>
         <p className="text-winc-pink text-center  text-xl">
-          Tried and true methods from our network of members to break into tech.
+          {description}
         </p>
 
         {/* Smaller Rectangle */}
         <div className="bg-winc-pink w-187 h-57 mt-4 rounded-lg flex items-center justify-center">
-          <p className="text-winc-yellow text-3xl justify-center font-light">
-            YOUTUBE
-          </p>
+          <div className="text-winc-yellow text-3xl justify-center font-light">
+            {socialMedia}
+          </div>
         </div>
       </div>
-    </div>
   );
 };
 
