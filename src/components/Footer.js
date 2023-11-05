9ec06cd3199f672656bab54e45682c81543b7662
Jonathan Le <jonathanle062303@gmail.com>
2023-11-04 18:44:59 -0700

    Added socials, fixed borders and padding

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,5 @@
 import logo from "@/public/assets/whiteLogo.webp";
+import Socials from "@/data/Socials";
 import Image from "next/image";
 import Link from "next/link";
 import Row from "react-bootstrap/Row";
@@ -6,38 +7,48 @@ import Col from "react-bootstrap/Col";
 
 const Footer = () => {
   return (
-    <div className="bg-winc-pink flex w-full justify-between items-end p-8 outline outline-8 outline-winc-pink outline-offset-8 my-8 h-full">
-      <Image src={logo} alt="logo" />
-      <Row className="flex text-winc-white justify-between w-6/12">
-        <Col className="flex flex-col space-y-4">
-          <div className="text-3xl font-semibold">PEOPLE</div>
-          <Link href="/about">
-            <div className="text-xl">About</div>
-          </Link>
-          <Link href="/board">
-            <div className="text-xl">Board</div>
-          </Link>
-        </Col>
-        <Col className="flex flex-col space-y-4">
-          <div className="text-3xl font-semibold">JOIN</div>
-          <Link href="/events">
-            <div className="text-xl">Events</div>
-          </Link>
-          <Link href="https://docs.google.com/forms/d/e/1FAIpQLSe5M-kEntAmPHnHNcerN6F8G5eevJs6zQJgEt3X3hcj_eNV3w/viewform">
-            <div className="text-xl">Membership</div>
-          </Link>
-        </Col>
-        <Col className="flex flex-col space-y-4">
-          <div className="text-3xl font-semibold">Stay In Touch</div>
-          {/* <Link href=""> */}
-          <div className="text-xl">Slack</div>
-          {/* </Link> */}
-          <Link href="mailto:wincucr@gmail.com">
-            <div className="text-xl">wincucr@gmail.com</div>
-          </Link>
-        </Col>
-        <Col className="">{/* Socials data go here */}</Col>
-      </Row>
+    <div className="bg-winc-white border-t-8 border-winc-pink">
+      <div className="bg-winc-pink flex w-full justify-between items-end p-8 pt-28 border-t-8 border-winc-white">
+        <Image src={logo} alt="logo" />
+        <Row className="flex text-winc-white justify-between items-end w-8/12">
+          <Col className="flex flex-col space-y-4 min-w-fit mb-4">
+            <div className="text-4xl font-semibold">PEOPLE</div>
+            <Link href="/about">
+              <div className="text-2xl font-light">About</div>
+            </Link>
+            <Link href="/board">
+              <div className="text-2xl font-light">Board</div>
+            </Link>
+          </Col>
+          <Col className="flex flex-col space-y-4 min-w-fit mb-4">
+            <div className="text-4xl font-semibold">JOIN</div>
+            <Link href="/events">
+              <div className="text-2xl font-light">Events</div>
+            </Link>
+            <Link href="https://docs.google.com/forms/d/e/1FAIpQLSe5M-kEntAmPHnHNcerN6F8G5eevJs6zQJgEt3X3hcj_eNV3w/viewform">
+              <div className="text-2xl font-light">Membership</div>
+            </Link>
+          </Col>
+          <Col className="flex flex-col space-y-4 min-w-fit mb-4">
+            <div className="text-4xl font-semibold">Stay In Touch</div>
+            {/* <Link href=""> */}
+            <div className="text-2xl font-light">Slack</div>
+            {/* </Link> */}
+            <Link href="mailto:wincucr@gmail.com">
+              <div className="text-2xl font-light">wincucr@gmail.com</div>
+            </Link>
+          </Col>
+          <Col className="flex flex-col space-y-2 items-end">
+            {Socials.map((social, index) => {
+              return (
+                <Link href={social.path} key={index}>
+                  {social.image}
+                </Link>
+              );
+            })}
+          </Col>
+        </Row>
+      </div>
     </div>
   );
 };
--- src/data/Socials.js
+++ src/data/Socials.js
@@ -1,42 +1,42 @@
 import {
   FaInstagram,
-  MdEmail,
   FaGithub,
   FaSlack,
   FaLinkedinIn,
   FaYoutube,
 } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
 
 const Socials = [
   {
     path: "https://www.instagram.com/wincucr/",
     title: "Instagram",
-    image: <FaInstagram className="text-5xl"></FaInstagram>,
+    image: <FaInstagram className="text-4xl"></FaInstagram>,
   },
   {
     path: "https://mail.google.com",
     title: "Email",
-    image: <MdEmail className="text-5xl"></MdEmail>,
+    image: <MdEmail className="text-4xl"></MdEmail>,
   },
   {
     path: "https://github.com",
     title: " Github",
-    image: <FaGithub className="text-5xl"></FaGithub>,
+    image: <FaGithub className="text-4xl"></FaGithub>,
   },
   {
     path: "https://app.slack.com/",
     title: "Slack",
-    image: <FaSlack className="text-5xl"></FaSlack>,
+    image: <FaSlack className="text-4xl"></FaSlack>,
   },
   {
     path: "https://www.linkedin.com",
     title: "LinkdIn",
-    image: <FaLinkedinIn className="text-5xl"></FaLinkedinIn>,
+    image: <FaLinkedinIn className="text-4xl"></FaLinkedinIn>,
   },
   {
     path: "https://www.youtube.com",
     title: "Youtube",
-    image: <FaYoutube className="text-5xl"></FaYoutube>,
+    image: <FaYoutube className="text-4xl"></FaYoutube>,
   },
 ];
 
