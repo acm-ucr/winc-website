5b36d91a3d4499bdd9be578745f0c61391874149
Roshini <roshinila@gmail.com>
2023-11-06 20:30:44 -0800

    Made changes to Service.js and created the trio

--- src/components/home/Service.js
+++ src/components/home/Service.js
@@ -1,21 +1,34 @@
 "use client";
 import React from "react";
-const Service = ({ icon, name, description, socialMedia }) => {
+const Service = ({
+  icon,
+  topic,
+  textColor,
+  description,
+  media,
+  mediaBG,
+  mediaText,
+  backgroundColor,
+}) => {
   return (
-    <div className="bg-winc-yellow w-11/12 h-96 rounded-lg flex items-center justify-center">
-      <div className="flex flex-column items-center w-11/12">
-        <div className="text-winc-pink" size="45" /> {icon}{" "}
-      </div>
-      <h1 className="text-winc-pink  text-center text-3xl mb-4 justify-center font-bold">
-        {name}
+    <div
+      className={` ${backgroundColor} p-6 w-11/12 rounded-lg flex items-center justify-center flex-col`}
+    >
+      {icon}
+      <h1
+        className={` ${textColor} text-center text-3xl mb-4 mt-2 justify-center font-bold`}
+      >
+        {topic}
       </h1>
-      <p className="text-winc-pink text-center  text-xl">{description}</p>
+      <p className={` ${textColor} text-center font-thin text-xl`}>
+        {description}
+      </p>
 
       {/* Smaller Rectangle */}
-      <div className="bg-winc-pink w-5/12 h-3/6 mt-4 px-8 rounded-lg flex items-center justify-center">
-        <div className="text-winc-yellow text-3xl justify-center font-light">
-          {socialMedia}
-        </div>
+      <div
+        className={` ${mediaBG} ${mediaText} w-8/12 h-1/6 rounded-md px-4 mt-4 flex text-4xl items-center font-light justify-center"`}
+      >
+        {media}
       </div>
     </div>
   );
---
+++ src/components/home/Services.js
@@ -0,0 +1,51 @@
+import React from "react";
+import { Row } from "react-bootstrap";
+import { Col } from "react-bootstrap";
+import Service from "./Service";
+import { MdMic } from "react-icons/md";
+const Services = () => {
+  return (
+    <div className="flex justify-center">
+      <Row className="w-10/12 flex justify-center">
+        <Col>
+          <Service
+            icon={<MdMic className="text-winc-yellow" size={45} />}
+            topic="CONVERSATIONS"
+            textColor="text-winc-yellow"
+            description="Discuss health and well-being with Let's Talk and catch up with WINC alumni on our Podcast"
+            media="LISTEN"
+            mediaBG="bg-winc-yellow"
+            mediaText="text-winc-black"
+            backgroundColor="bg-winc-black"
+          />
+        </Col>
+        <Col>
+          <Service
+            icon={<MdMic className="text-winc-yellow" size={45} />}
+            topic="CONVERSATIONS"
+            textColor="text-winc-yellow"
+            description="Discuss health and well-being with Let's Talk and catch up with WINC alumni on our Podcast"
+            media="LISTEN"
+            mediaBG="bg-winc-yellow"
+            mediaText="text-winc-black"
+            backgroundColor="bg-winc-black"
+          />
+        </Col>
+        <Col>
+          <Service
+            icon={<MdMic className="text-winc-yellow" size={45} />}
+            topic="CONVERSATIONS"
+            textColor="text-winc-yellow"
+            description="Discuss health and well-being with Let's Talk and catch up with WINC alumni on our Podcast"
+            media="LISTEN"
+            mediaBG="bg-winc-yellow"
+            mediaText="text-winc-black"
+            backgroundColor="bg-winc-black"
+          />
+        </Col>
+      </Row>
+    </div>
+  );
+};
+
+export default Services;
