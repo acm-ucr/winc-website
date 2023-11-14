6b475b3476e322c8df8f38b50bcd64bef96a2ed4
shreyamohan <mohanshreya1@gmail.com>
2023-11-13 18:57:27 -0800

    need help

--- src/components/board/Officer.js
+++ src/components/board/Officer.js
@@ -16,21 +16,24 @@ const Officer = ({
 }) => {
   return (
     <div className="bg-winc-beige px-4 pt-4 pb-2 w-full shadow-[12px_-10px_0px_2px] shadow-winc-orange">
-      <div className="w-11/12 h-3/6">
-        {" "}
+      <div className="flex flex-col space-x-3">
+        <a href={linkedin} className="text-winc-pink">
+          <LiaLinkedinIn size="42" />
+        </a>
+        <a href={"mailto:" + email} className="text-winc-pink">
+          <IoMdMail size="42" />
+        </a>
         <Image src={image} width="256" height="256" alt={name} />
       </div>
+      {/* <div className="w-11/12 h-3/6">
+        {" "} */}
+
+      {/* </div> */}
+
       <div className="text-winc-pink font-urbanist p-2">
         <div className="text-4xl font-semibold whitespace-nowrap">{name}</div>
         <div className="text-2xl ml-2">{position}</div>
-        <div className="flex flex-row space-x-3">
-          <a href={linkedin} className="text-winc-pink">
-            <LiaLinkedinIn size="42" />
-          </a>
-          <a href={"mailto:" + email} className="text-winc-pink">
-            <IoMdMail size="42" />
-          </a>
-        </div>
+
         <div className="text-lg -space-y-1">
           <div>
             {year} year · {major}
