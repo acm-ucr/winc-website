27ac329a564caeb64947cbc09f5b5e2a8fa66661
Jonathan Le <jonathanle062303@gmail.com>
2023-11-15 13:05:09 -0800

    Fixed variable name

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,9 @@
 import Title from "@/components/Title";
-import Circle from "@/components/home/Circle";
-import about2 from "@/public/assets/about2.webp";
 // import Stats from "@/components/home/Stats";
 const Home = () => {
   return (
     <>
       <Title text="Home" />
-      <Circle width="80" height="80" image={about2} />
     </>
   );
 };
--- src/components/home/Circle.js
+++ src/components/home/Circle.js
@@ -1,11 +1,30 @@
 import Image from "next/image";
 
 const Circle = ({ width, height, clip, image }) => {
+  let rectClip;
+  if (clip === "left") {
+    rectClip = { clipPath: "polygon(0% 0%, 0% 100%, 50% 100%, 50% 0%)" };
+  } else {
+    rectClip = { clipPath: "polygon(100% 0%, 100% 100%, 50% 100%, 50% 0%)" };
+  }
+
   return (
-    <div className={"w-" + width + " h-" + height}>
-      <div className="flex items-center bg-winc-pink rounded-full outline outline-4 outline-offset-8 outline-winc-pink w-full h-full overflow-hidden">
-        <Image src={image} className="" />
-      </div>
+    <div
+      className="flex justify-center items-center bg-winc-pink rounded-full outline outline-4 outline-offset-8 outline-winc-pink w-full h-full relative"
+      style={{ width: width + "px", height: height + "px" }}
+    >
+      <Image
+        src={image}
+        width={width * 1.1}
+        className="max-w-none"
+        style={{ clipPath: "circle(53.4%)" }}
+      />
+      <Image
+        src={image}
+        width={width * 1.1}
+        className="absolute rounded-3xl max-w-none"
+        style={rectClip}
+      />
     </div>
   );
 };
