e0f85ffbdb3ca6444c126debf49c892b487e168f
Mailyn Nguyen <ngu.mailyn.yen@gmail.com>
2023-11-15 13:07:29 -0800

    fix merge conflict

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,7 +1,6 @@
 import We from "@/components/about/We";
 import What from "@/components/about/What";
 import Services from "@/components/about/Services";
-
 const About = () => {
   return (
     <>
