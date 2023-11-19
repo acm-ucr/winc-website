a86d9338346e21c42e2ad847ee5c4d284806a022
Mailyn Nguyen <ngu.mailyn.yen@gmail.com>
2023-11-19 12:40:57 -0800

    fix comments

--- src/components/about/Services.js
+++ src/components/about/Services.js
@@ -13,22 +13,34 @@ const Services = () => {
       <div className="flex text-center justify-center pt-2 pb-16">
         <Title text="Become a Member" textcolor="text-winc-white" />
       </div>
-      <Row className="flex flex-col md:flex-row justify-center w-11/12 pb-16">
-        <Col className="flex flex-col py-2 items-center justify-center text-center">
+      <Row className="flex justify-center w-11/12 pb-16">
+        <Col
+          sm={6}
+          md={4}
+          className="flex flex-col items-center justify-center text-center"
+        >
           <Service
             icon={<IoPersonSharp size={40} />}
             info="Meetings & Events"
             description="Sign-in sheets will be sent out at the beginning of every meeting and event.Sign in so we can track attendance and give you access to our newsletters."
           />
         </Col>
-        <Col className="flex flex-col py-2 items-center justify-center text-center">
+        <Col
+          sm={6}
+          md={4}
+          className="flex flex-col items-center justify-center text-center"
+        >
           <Service
             icon={<IoPersonSharp size={40} />}
             info="Join our Slack"
             description="Join our Slack to communicate with us and have access to all our channels including #opportunities — a great resouce for seeking internships and on-campus jobs."
           />
         </Col>
-        <Col className="flex flex-col py-2 items-center justify-center text-center">
+        <Col
+          sm={6}
+          md={4}
+          className="flex flex-col items-center justify-center text-center"
+        >
           <Service
             icon={<IoPersonSharp size={40} />}
             info="Follow our Socials"
