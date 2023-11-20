634a48d7f6b26881897e6a48afc3a847958cc6f0
shreyamohan <mohanshreya1@gmail.com>
2023-11-20 11:17:45 -0800

    need help with spacing

--- src/components/board/Officer.js
+++ src/components/board/Officer.js
@@ -2,6 +2,8 @@ import React from "react";
 import Image from "next/image";
 import { LiaLinkedinIn } from "react-icons/lia";
 import { IoMdMail } from "react-icons/io";
+import { Row } from "react-bootstrap";
+import { Col } from "react-bootstrap";
 
 const Officer = ({
   image,
@@ -12,35 +14,37 @@ const Officer = ({
   year,
   major,
   pronouns,
-  starsign,
+  hobbies,
 }) => {
   return (
     <div className="bg-winc-beige px-4 pt-4 pb-2 w-full shadow-[12px_-10px_0px_2px] shadow-winc-orange">
-      <div className="flex flex-col space-x-3">
-        <a href={linkedin} className="text-winc-pink">
-          <LiaLinkedinIn size="42" />
-        </a>
-        <a href={"mailto:" + email} className="text-winc-pink">
-          <IoMdMail size="42" />
-        </a>
-        <Image src={image} width="256" height="256" alt={name} />
-      </div>
-      {/* <div className="w-11/12 h-3/6">
-        {" "} */}
-
-      {/* </div> */}
-
       <div className="text-winc-pink font-urbanist p-2">
-        <div className="text-4xl font-semibold whitespace-nowrap">{name}</div>
-        <div className="text-2xl ml-2">{position}</div>
+        <Row className="space-x-3 bg-winc-pink w-full h-full">
+          <Col className="space-y-3 bg-winc-white w-2/12">
+            <LiaLinkedinIn size="34" />
+            <IoMdMail size="34" />
+          </Col>
+          <Col className="bg-winc-black w-10/12 h-8/12">
+            <Image src={image} width="400" height="400"></Image>
+          </Col>
+        </Row>
 
-        <div className="text-lg -space-y-1">
-          <div>
-            {year} year · {major}
-          </div>
-          <div>Pronouns: {pronouns}</div>
-          <div>Star Sign: {starsign}</div>
-        </div>
+        <Row className="w-full">
+          <Col></Col>
+          <Col className="bg-winc-orange w-9/12">
+            <div className="text-2xl font-semibold whitespace-nowrap">
+              {name}
+            </div>
+            <div className="text-2xl ml-2">{position}</div>
+            <div className="text-lg -space-y-1">
+              <div>
+                {year} year · {major}
+              </div>
+              <div>Pronouns: {pronouns}</div>
+              <div>Hobbies: {hobbies}</div>
+            </div>
+          </Col>
+        </Row>
       </div>
     </div>
   );
