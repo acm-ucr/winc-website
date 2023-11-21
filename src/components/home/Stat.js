2c43d6fc8f30e77e992181047e9d42e632342413
Sahana Anand <sahanaanand0516@gmail.com>
2023-11-20 16:29:22 -0800

    stats components are in columns when on mobile, but within stats components the icons remain next to the words

--- src/components/home/Stat.js
+++ src/components/home/Stat.js
@@ -1,15 +1,16 @@
 "use client";
 import React from "react";
+
 // style the component here
 const Stat = ({ icon, stat, type }) => {
   return (
     <div
-      className={`flex flex-row w-full h-full items-center rounded-full border-8 border-winc-yellow ring-8 ring-winc-orange mx-4 text-winc-pink bg-winc-white py-3`}
+      className={`flex flex-row w-full h-full items-center rounded-full border-8 border-winc-yellow ring-8 ring-winc-orange mx-4 text-winc-pink bg-winc-white py-3 sm: flex flex-row`}
     >
-      <div className=" w-5/12 ml-6">{icon}</div>
+      <div className="ml-6 mr-0 w-5/12 ">{icon}</div>
       <div className="flex flex-col justify-center w-8/12">
-        <div className=" text-6xl font-bold">{stat}</div>
-        <div className="text-winc-black text-2xl font-normal text-left">
+        <div className=" text-left text-lg sm:text-6xl font-bold">{stat}</div>
+        <div className=" text-winc-black text-lg sm: text-2xl font-normal text-left">
           {type}
         </div>
       </div>
--- src/components/home/Stats.js
+++ src/components/home/Stats.js
@@ -3,6 +3,8 @@ import React from "react";
 import { FaChalkboardTeacher } from "react-icons/fa";
 import { IoPersonSharp } from "react-icons/io5";
 import { FaUserGraduate } from "react-icons/fa";
+import { Row } from "react-bootstrap";
+import { Col } from "react-bootstrap";
 
 import Stat from "./Stat";
 
@@ -12,19 +14,32 @@ const Stats = () => {
   return (
     <div className="flex justify-center items-center bg-winc-pink w-full p-16 outline outline-8 outline-winc-pink outline-offset-8 my-4">
       <div className=""></div>
-      <div className="bg-winc-pink flex flex-row w-11/12">
-        <Stat
-          icon={<FaChalkboardTeacher size="84px" />}
-          stat="100+"
-          type="Workshops"
-        />
-        <Stat icon={<IoPersonSharp size="70px" />} stat="500+" type="Members" />
-        <Stat
-          icon={<FaUserGraduate size="70px" />}
-          stat="1000+"
-          type="Alumni"
-        />
-      </div>
+      {/* <div className="bg-winc-pink flex flex-row w-11/12"> */}
+      <Row className="flex justify-center w-11/12 ">
+        <Col className="flex flex-col items-center justify-center text-center mb-12">
+          <Stat
+            icon={<FaChalkboardTeacher size="84px" />}
+            stat="100+"
+            type="Workshops"
+          />
+        </Col>
+        <Col className="flex flex-col items-center justify-center text-center mb-12">
+          <Stat
+            icon={<IoPersonSharp size="70px" />}
+            stat="500+"
+            type="Members"
+          />
+        </Col>
+        <Col className="flex flex-col items-center justify-center text-center mb-12">
+          <Stat
+            icon={<FaUserGraduate size="70px" />}
+            stat="1000+"
+            type="Alumni"
+          />
+        </Col>
+      </Row>
+
+      {/* </div> */}
     </div>
   );
 };
