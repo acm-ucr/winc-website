adab0d4d862ae0b46126b36c99051ffdc1da3cbd
Jonathan Le <jonathanle062303@gmail.com>
2023-12-01 20:10:52 -0800

    New Officer Card Design

--- src/components/board/Officer.js
+++ src/components/board/Officer.js
@@ -1,5 +1,7 @@
 import React from "react";
 import Image from "next/image";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 import { LiaLinkedinIn } from "react-icons/lia";
 import { IoMdMail } from "react-icons/io";
 
@@ -15,31 +17,28 @@ const Officer = ({
   starsign,
 }) => {
   return (
-    <div className="bg-winc-beige px-4 pt-4 pb-2 w-full shadow-[12px_-10px_0px_2px] shadow-winc-orange">
-      <div className="w-11/12 h-3/6">
-        {" "}
-        <Image src={image} width="256" height="256" alt={name} />
-      </div>
-      <div className="text-winc-pink font-urbanist p-2">
-        <div className="text-4xl font-semibold whitespace-nowrap">{name}</div>
-        <div className="text-2xl ml-2">{position}</div>
-        <div className="flex flex-row space-x-3">
-          <a href={linkedin} className="text-winc-pink">
-            <LiaLinkedinIn size="42" />
-          </a>
-          <a href={"mailto:" + email} className="text-winc-pink">
-            <IoMdMail size="42" />
-          </a>
-        </div>
-        <div className="text-lg -space-y-1">
+    <Row className="flex flex-row bg-winc-beige pt-4 pb-3 w-11/12 max-w-max shadow-[12px_-10px_0px_2px] shadow-winc-orange">
+      <Col className="flex flex-col mt-8 space-y-4" lg={2}>
+        <a href={linkedin} className="text-winc-pink">
+          <LiaLinkedinIn size="38" />
+        </a>
+        <a href={"mailto:" + email} className="text-winc-pink">
+          <IoMdMail size="38" />
+        </a>
+      </Col>
+      <Col className="text-winc-pink font-urbanist whitespace-nowrap" lg={9}>
+        <Image src={image} width={256} height={256} alt={name} />
+        <div className="text-3xl mt-2 font-semibold">{name}</div>
+        <div className="text-2xl">{position}</div>
+        <div className="text-md -space-y-1 mt-1">
           <div>
             {year} year · {major}
           </div>
           <div>Pronouns: {pronouns}</div>
           <div>Star Sign: {starsign}</div>
         </div>
-      </div>
-    </div>
+      </Col>
+    </Row>
   );
 };
 
