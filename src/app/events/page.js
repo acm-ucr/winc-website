b1a72690a4245938f62f24760f0ac984d91f9420
Mailyn Nguyen <ngu.mailyn.yen@gmail.com>
2023-12-03 14:20:51 -0800

    event bugs

--- src/app/events/page.js
+++ src/app/events/page.js
@@ -4,10 +4,12 @@ import CalendarEvents from "@/components/events/CalendarEvents";
 const Events = () => {
   return (
     <>
-      <div className="flex justify-center items-center bg-winc-pink w-full p-8 outline outline-8 outline-winc-pink outline-offset-8 my-4">
+      <div className="flex justify-center items-center bg-winc-pink w-full p-4 outline outline-8 outline-winc-pink outline-offset-4 my-4">
         <Title text="Programs and Events" textcolor="text-winc-white" />
       </div>
-      <CalendarEvents />
+      <div className="pt-16">
+        <CalendarEvents />
+      </div>
       <Upcoming />
     </>
   );
