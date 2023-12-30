4fc5c8c3851b23bb62c04e4195d938665692e66f
Raidah Fairooz <rfair008@ucr.edu>
2023-12-29 18:12:20 -0800

    fixed config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false,
+  output: "export",
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    loader: "akamai",
+    path: "",
+    unoptimized: true,
+  },
+  basePath: "",
+  assetPrefix: "",
 };
+
 module.exports = nextConfig;
