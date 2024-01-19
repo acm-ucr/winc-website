4acb06a700afbb9a3bafbcea405d4252328608f9
Raidah Fairooz <rfair008@ucr.edu>
2024-01-19 10:29:56 -0800

    fixed calednar toolbar

--- src/components/events/CustomToolbar.js
+++ src/components/events/CustomToolbar.js
@@ -1,15 +1,30 @@
-import React from "react";
 import { FaArrowRight, FaArrowLeft } from "react-icons/fa";
-
+const monthNames = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
 const CustomToolbar = (event) => {
   return (
     <div className="flex justify-center items-center font-bold text-3xl">
       <FaArrowLeft
-        onClick={() => event.onNavigate("PREV")}
+        onClick={() => {
+          console.log("hi");
+          event.onNavigate("PREV");
+        }}
         className="text-winc-pink hover:cursor-pointer "
       />
       <div className="text-center text-2xl md:text-5xl p-4 text-winc-black">
-        {event.date.toLocaleDateString("en-US", { month: "long" })}
+        {monthNames[event.date.getMonth()]}
         &nbsp;
         {event.date.getFullYear() % 100}
       </div>
