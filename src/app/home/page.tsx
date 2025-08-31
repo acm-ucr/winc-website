cf8b376046bd7cb1c0cabe9a945ef1aa2e208b92
stanleylew5 <stanley43595@gmail.com>
2025-08-31 01:05:22 -0700

    change route away from home

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,11 +1,9 @@
-import GrowAndThrive from "@/components/home/growandthrive";
 import Landing from "@/components/home/landing";
 
 const Page = () => {
   return (
     <div>
       <Landing />
-      <GrowAndThrive />
     </div>
   );
 };
