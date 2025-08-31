1e3279a38e215b6b3e1e35bd72e2af9679ba7cde
ALE00111 <austintle56@gmail.com>
2025-08-31 10:13:11 -0700

    minor change

--- src/components/events/title.tsx
+++ src/components/events/title.tsx
@@ -6,8 +6,8 @@ const Title = ({ title }: TitleProps) => {
   return (
     <div className="flex w-1/6 flex-col items-center text-4xl font-bold">
       {title}
-      <div className="mt-3 h-2 w-4/5 bg-winc-red-700" />
-      <div className="h-2 w-2/3 bg-winc-red-200" />
+      <div className="mt-3 w-4/5 bg-winc-red-700 py-1" />
+      <div className="w-2/3 bg-winc-red-200 py-1" />
     </div>
   );
 };
