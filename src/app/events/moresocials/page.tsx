4901061583883f87a01414c52103469859cdc5e1
Charleen Chen <charleenc485@gmail.com>
2025-08-31 11:44:50 -0700

    added more socials

---
+++ src/app/events/moresocials/page.tsx
@@ -0,0 +1,11 @@
+import MoreSocials from "@/components/events/moresocials/moreSocials";
+
+const Page = () => {
+  return (
+    <div>
+      <MoreSocials />
+    </div>
+  );
+};
+
+export default Page;
