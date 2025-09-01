7898e8ffca8b82095ba8413deaa66fa31b358ef3
Kevin Loritsch <kloritsch58@gmail.com>
2025-08-31 17:17:23 -0700

    remove layout tweak

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import { ReactQueryClientProvider } from "@/utils/react-query";
 import Navbar from "@/components/navbar";
 import Footer from "@/components/footer";
 
-const urbanist = Urbanist({ subsets: ["latin"], weight: ["400", "700"] });
+const urbanist = Urbanist({ subsets: ["latin"] });
 
 export const metadata = {
   title: "WINC @ UCR",
