6f55e7877f1ecd4707c1f3e34fc4448d29e37355
stanleylew5 <stanley43595@gmail.com>
2025-09-01 22:01:55 -0700

    format

--- src/app/events/rosehack/page.tsx
+++ src/app/events/rosehack/page.tsx
@@ -3,13 +3,13 @@ import hearts from "@/public/events/hearts.webp"; */
 
 const Page = () => {
   return (
-    
     <div>
-{/* <LookDown
+      {/* <LookDown
       titleText="What is Rose Hack?"
       sideImage={hearts}
       content="Rose Hack is a hackathon dedicated to uplifting underrepesented communities in the engineering field. The hackathon encourages participants to create projects, attend workshops, enjoy games, and talk to industry professionals! The hackathon has been around for ___ years and continues to thrive."
-    /> */}ss
+    /> */}
+      ss
     </div>
   );
 };
--- src/app/events/socials/page.tsx
+++ src/app/events/socials/page.tsx
@@ -4,7 +4,7 @@ import hearts from "@/public/events/hearts.webp"; */
 const Page = () => {
   return (
     <div>
-    {/* <LookDown
+      {/* <LookDown
       titleText="What kind of socials do we host?"
       sideImage={hearts}
       content="We host all kinds of socials! We host game days, workshops, general meetings, etc. We aim to make everyone feel included through these events."
