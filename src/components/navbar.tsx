78defbcd6eda8a6b33646c427a9590cce20569c3
stanleylew5 <stanley43595@gmail.com>
2025-10-20 12:01:11 -0700

    add padding

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -107,7 +107,7 @@ const Navbar = () => {
         className="absolute left-0 top-0 flex w-full flex-col items-center justify-center bg-white shadow-lg md:hidden"
         variants={sidebarVariants}
       >
-        <motion.ul className="list-none space-y-6 pt-4 text-2xl sm:pt-0">
+        <motion.ul className="list-none space-y-6 pb-4 pt-4 text-2xl sm:pt-0">
           {navigations.map(({ link, name, className }, index) => (
             <motion.li
               key={index}
